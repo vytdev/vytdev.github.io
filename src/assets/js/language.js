b087c0838e92f8876958cdce3e9391740f0f1fbd
Vincent Yanzee J. Tan <vytdev@gmail.com>
2024-07-05 10:32:03 +0800

    use 2-space instead of tabs :|

--- src/assets/js/language.js
+++ src/assets/js/language.js
@@ -10,253 +10,253 @@ Thank you NLP and Porter Stemmer!
 ================================================================ */
 
 var languageData = (function() {
-	var // language data
-		langEn = {},
-		// used in normalization
-		codePoints = /[\u0300-\u036f]/g,
-		// canonical decomposition
-		form = "NFD",
-		punctuations = /[\s~`’‘|^°{}[\]()<>\\%@#$&\-+=/*"':;!?.,]+/,
-		// for quoted contractions
-		replacements = [
-			[/n't([ ,:;.!?]|$)/gi, ' not '],
-			[/can't([ ,:;.!?]|$)/gi, 'can not '],
-			[/'ll([ ,:;.!?]|$)/gi, ' will '],
-			[/'s([ ,:;.!?]|$)/gi, ' is '],
-			[/'re([ ,:;.!?]|$)/gi, ' are '],
-			[/'ve([ ,:;.!?]|$)/gi, ' have '],
-			[/'m([ ,:;.!?]|$)/gi, ' am '],
-			[/'d([ ,:;.!?]|$)/gi, ' had ']
-		],
-		// for unquoted contractions
-		contractionsBase = {
-			cannot: ['can', 'not'],
-			gonna: ['going', 'to'],
-			wanna: ['want', 'to']
-		};
+  var // language data
+    langEn = {},
+    // used in normalization
+    codePoints = /[\u0300-\u036f]/g,
+    // canonical decomposition
+    form = "NFD",
+    punctuations = /[\s~`’‘|^°{}[\]()<>\\%@#$&\-+=/*"':;!?.,]+/,
+    // for quoted contractions
+    replacements = [
+      [/n't([ ,:;.!?]|$)/gi, ' not '],
+      [/can't([ ,:;.!?]|$)/gi, 'can not '],
+      [/'ll([ ,:;.!?]|$)/gi, ' will '],
+      [/'s([ ,:;.!?]|$)/gi, ' is '],
+      [/'re([ ,:;.!?]|$)/gi, ' are '],
+      [/'ve([ ,:;.!?]|$)/gi, ' have '],
+      [/'m([ ,:;.!?]|$)/gi, ' am '],
+      [/'d([ ,:;.!?]|$)/gi, ' had ']
+    ],
+    // for unquoted contractions
+    contractionsBase = {
+      cannot: ['can', 'not'],
+      gonna: ['going', 'to'],
+      wanna: ['want', 'to']
+    };
 
-	// collection of english stopwords
-	langEn.stopwords = ["about", "above", "after", "again", "all", "also", "am", "an", "and",
-		"another", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below",
-		"between", "both", "but", "by", "came", "can", "cannot", "come", "could", "did", "do",
-		"does", "doing", "during", "each", "few", "for", "from", "further", "get", "got", "has",
-		"had", "he", "have", "her", "here", "him", "himself", "his", "how", "if", "in", "into",
-		"is", "it", "its", "itself", "like", "make", "many", "me", "might", "more", "most", "much",
-		"must", "my", "myself", "never", "now", "of", "on", "only", "or", "other", "our", "ours",
-		"ourselves", "out", "over", "own", "said", "same", "see", "should", "since", "so", "some",
-		"still", "such", "take", "than", "that", "the", "their", "theirs", "them", "themselves",
-		"then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until",
-		"up", "very", "was", "way", "we", "well", "were", "what", "where", "when", "which", "while",
-		"who", "whom", "with", "would", "why", "you", "your", "yours", "yourself", "a", "b", "c",
-		"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-		"w", "x", "y", "z", "_"
-	];
+  // collection of english stopwords
+  langEn.stopwords = ["about", "above", "after", "again", "all", "also", "am", "an", "and",
+    "another", "any", "are", "as", "at", "be", "because", "been", "before", "being", "below",
+    "between", "both", "but", "by", "came", "can", "cannot", "come", "could", "did", "do",
+    "does", "doing", "during", "each", "few", "for", "from", "further", "get", "got", "has",
+    "had", "he", "have", "her", "here", "him", "himself", "his", "how", "if", "in", "into",
+    "is", "it", "its", "itself", "like", "make", "many", "me", "might", "more", "most", "much",
+    "must", "my", "myself", "never", "now", "of", "on", "only", "or", "other", "our", "ours",
+    "ourselves", "out", "over", "own", "said", "same", "see", "should", "since", "so", "some",
+    "still", "such", "take", "than", "that", "the", "their", "theirs", "them", "themselves",
+    "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until",
+    "up", "very", "was", "way", "we", "well", "were", "what", "where", "when", "which", "while",
+    "who", "whom", "with", "would", "why", "you", "your", "yours", "yourself", "a", "b", "c",
+    "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+    "w", "x", "y", "z", "_"
+  ];
 
-	// normalizes a string
-	langEn.normalize = function(s) {
-		return s.normalize(form).replace(codePoints, "")
-	}
+  // normalizes a string
+  langEn.normalize = function(s) {
+    return s.normalize(form).replace(codePoints, "")
+  }
 
-	// tokenizes a string
-	langEn.tokenize = function(s) {
-		s = s.toLowerCase();
-		var idx, punctSplit, word, raw = [], finalVals = [];
+  // tokenizes a string
+  langEn.tokenize = function(s) {
+    s = s.toLowerCase();
+    var idx, punctSplit, word, raw = [], finalVals = [];
 
-		// expand single quoted word
-		for (idx = 0; idx < replacements.length; idx++) {
-			s = s.replace(replacements[idx][0], replacements[idx][1]);
-		}
+    // expand single quoted word
+    for (idx = 0; idx < replacements.length; idx++) {
+      s = s.replace(replacements[idx][0], replacements[idx][1]);
+    }
 
-		// extract by punctuations
-		punctSplit = s.split(punctuations);
+    // extract by punctuations
+    punctSplit = s.split(punctuations);
 
-		// expand contracted words
-		for (idx = 0; idx < punctSplit.length; idx++) {
-			word = punctSplit[idx];
-			if (contractionsBase[word]) {
-				Array.prototype.push.apply(raw, contractionsBase[word]);
-			}
-			else raw.push(punctSplit[idx]);
-		};
+    // expand contracted words
+    for (idx = 0; idx < punctSplit.length; idx++) {
+      word = punctSplit[idx];
+      if (contractionsBase[word]) {
+        Array.prototype.push.apply(raw, contractionsBase[word]);
+      }
+      else raw.push(punctSplit[idx]);
+    };
 
-		// filter empty entries
-		for (idx = 0; idx < raw.length; idx++) {
-			if (raw[idx].length) finalVals.push(raw[idx]);
-		}
+    // filter empty entries
+    for (idx = 0; idx < raw.length; idx++) {
+      if (raw[idx].length) finalVals.push(raw[idx]);
+    }
 
-		return finalVals;
-	}
+    return finalVals;
+  }
 
-	// extract terms in the provided string
-	langEn.split = function(s) {
-		return langEn.tokenize(langEn.normalize(s));
-	}
+  // extract terms in the provided string
+  langEn.split = function(s) {
+    return langEn.tokenize(langEn.normalize(s));
+  }
 
-	// returns true if s is a stopword
-	langEn.stop = function(s) {
-		return langEn.stopwords.includes(s);
-	}
+  // returns true if s is a stopword
+  langEn.stop = function(s) {
+    return langEn.stopwords.includes(s);
+  }
 
-	// Porter Stemmer
-	// Modified to fasten itself
-	var step2list = {
-		"ational": "ate",
-		"tional": "tion",
-		"enci": "ence",
-		"anci": "ance",
-		"izer": "ize",
-		"bli": "ble",
-		"alli": "al",
-		"entli": "ent",
-		"eli": "e",
-		"ousli": "ous",
-		"ization": "ize",
-		"ation": "ate",
-		"ator": "ate",
-		"alism": "al",
-		"iveness": "ive",
-		"fulness": "ful",
-		"ousness": "ous",
-		"aliti": "al",
-		"iviti": "ive",
-		"biliti": "ble",
-		"logi": "log"
-	},
-	step3list = {
-		"icate": "ic",
-		"ative": "",
-		"alize": "al",
-		"iciti": "ic",
-		"ical": "ic",
-		"ful": "",
-		"ness": ""
-	},
-	c = "[^aeiou]",
-	// consonant
-	v = "[aeiouy]",
-	// vowel
-	C = c + "[^aeiouy]*",
-	// consonant sequence
-	V = v + "[aeiou]*",
-	// vowel sequence
-	mgr0 = new RegExp("^(" + C + ")?" + V + C),
-	// [C]VC... is m>0
-	meq1 = new RegExp("^(" + C + ")?" + V + C + "(" + V + ")?$"),
-	// [C]VC[V] is m=1
-	mgr1 = new RegExp("^(" + C + ")?" + V + C + V + C),
-	// [C]VCVC... is m>1
-	s_v = new RegExp("^(" + C + ")?" + v),
-	// vowel in stem
-	// instantiate these regex once
-	ra = /^(.+?)(ss|i)es$/,
-	rb = /^(.+?)([^s])s$/,
-	rc = /^(.+?)eed$/,
-	rd = /^(.+?)(ed|ing)$/,
-	re = /.$/,
-	rf = /(at|bl|iz)$/,
-	rg = new RegExp("([^aeiouylsz])\\1$"),
-	rh = new RegExp("^" + C + v + "[^aeiouwxy]$"),
-	ri = /^(.+?)y$/,
-	rj = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/,
-	rk = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/,
-	rl = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/,
-	rm = /^(.+?)(s|t)(ion)$/,
-	rn = /^(.+?)e$/,
-	ro = /ll$/;
-	langEn.stem = function (w) {
-		var stem,
-		suffix,
-		firstch,
-		origword = w;
-		if (w.length < 3) {
-			return w;
-		}
-		firstch = w.substr(0, 1);
-		if (firstch == "y") {
-			w = firstch.toUpperCase() + w.substr(1);
-		}
-		// Step 1a
-		if (ra.test(w)) {
-			w = w.replace(ra, "$1$2");
-		} else if (rb.test(w)) {
-			w = w.replace(rb, "$1$2");
-		}
-		// Step 1b
-		if (rc.test(w)) {
-			var fp = rc.exec(w);
-			if (mgr0.test(fp[1])) {
-				w = w.replace(re, "");
-			}
-		} else if (rd.test(w)) {
-			var fp = rd.exec(w);
-			stem = fp[1];
-			if (s_v.test(stem)) {
-				w = stem;
-				if (rf.test(w)) {
-					w = w + "e";
-				} else if (rg.test(w)) {
-					w = w.replace(re, "");
-				} else if (rh.test(w)) {
-					w = w + "e";
-				}
-			}
-		}
-		// Step 1c
-		if (ri.test(w)) {
-			var fp = ri.exec(w);
-			stem = fp[1];
-			if (s_v.test(stem)) {
-				w = stem + "i";
-			}
-		}
-		// Step 2;
-		if (rj.test(w)) {
-			var fp = rj.exec(w);
-			stem = fp[1];
-			suffix = fp[2];
-			if (mgr0.test(stem)) {
-				w = stem + step2list[suffix];
-			}
-		}
-		// Step 3
-		if (rk.test(w)) {
-			var fp = rk.exec(w);
-			stem = fp[1];
-			suffix = fp[2];
-			if (mgr0.test(stem)) {
-				w = stem + step3list[suffix];
-			}
-		}
-		// Step 4
-		if (rl.test(w)) {
-			var fp = rl.exec(w);
-			stem = fp[1];
-			if (mgr1.test(stem)) {
-				w = stem;
-			}
-		} else if (rm.test(w)) {
-			var fp = rm.exec(w);
-			stem = fp[1] + fp[2];
-			if (mgr1.test(stem)) {
-				w = stem;
-			}
-		}
-		// Step 5
-		if (rn.test(w)) {
-			var fp = rn.exec(w);
-			stem = fp[1];
-			if (mgr1.test(stem) || (meq1.test(stem) && !(rh.test(stem)))) {
-				w = stem;
-			}
-		}
-		if (ro.test(w) && mgr1.test(w)) {
-			w = w.replace(re, "");
-		}
-		// and turn initial Y back to y
-		if (firstch == "y") {
-			w = firstch.toLowerCase() + w.substr(1);
-		}
-		return w;
-	};
-	return langEn;
+  // Porter Stemmer
+  // Modified to fasten itself
+  var step2list = {
+    "ational": "ate",
+    "tional": "tion",
+    "enci": "ence",
+    "anci": "ance",
+    "izer": "ize",
+    "bli": "ble",
+    "alli": "al",
+    "entli": "ent",
+    "eli": "e",
+    "ousli": "ous",
+    "ization": "ize",
+    "ation": "ate",
+    "ator": "ate",
+    "alism": "al",
+    "iveness": "ive",
+    "fulness": "ful",
+    "ousness": "ous",
+    "aliti": "al",
+    "iviti": "ive",
+    "biliti": "ble",
+    "logi": "log"
+  },
+  step3list = {
+    "icate": "ic",
+    "ative": "",
+    "alize": "al",
+    "iciti": "ic",
+    "ical": "ic",
+    "ful": "",
+    "ness": ""
+  },
+  c = "[^aeiou]",
+  // consonant
+  v = "[aeiouy]",
+  // vowel
+  C = c + "[^aeiouy]*",
+  // consonant sequence
+  V = v + "[aeiou]*",
+  // vowel sequence
+  mgr0 = new RegExp("^(" + C + ")?" + V + C),
+  // [C]VC... is m>0
+  meq1 = new RegExp("^(" + C + ")?" + V + C + "(" + V + ")?$"),
+  // [C]VC[V] is m=1
+  mgr1 = new RegExp("^(" + C + ")?" + V + C + V + C),
+  // [C]VCVC... is m>1
+  s_v = new RegExp("^(" + C + ")?" + v),
+  // vowel in stem
+  // instantiate these regex once
+  ra = /^(.+?)(ss|i)es$/,
+  rb = /^(.+?)([^s])s$/,
+  rc = /^(.+?)eed$/,
+  rd = /^(.+?)(ed|ing)$/,
+  re = /.$/,
+  rf = /(at|bl|iz)$/,
+  rg = new RegExp("([^aeiouylsz])\\1$"),
+  rh = new RegExp("^" + C + v + "[^aeiouwxy]$"),
+  ri = /^(.+?)y$/,
+  rj = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/,
+  rk = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/,
+  rl = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/,
+  rm = /^(.+?)(s|t)(ion)$/,
+  rn = /^(.+?)e$/,
+  ro = /ll$/;
+  langEn.stem = function (w) {
+    var stem,
+    suffix,
+    firstch,
+    origword = w;
+    if (w.length < 3) {
+      return w;
+    }
+    firstch = w.substr(0, 1);
+    if (firstch == "y") {
+      w = firstch.toUpperCase() + w.substr(1);
+    }
+    // Step 1a
+    if (ra.test(w)) {
+      w = w.replace(ra, "$1$2");
+    } else if (rb.test(w)) {
+      w = w.replace(rb, "$1$2");
+    }
+    // Step 1b
+    if (rc.test(w)) {
+      var fp = rc.exec(w);
+      if (mgr0.test(fp[1])) {
+        w = w.replace(re, "");
+      }
+    } else if (rd.test(w)) {
+      var fp = rd.exec(w);
+      stem = fp[1];
+      if (s_v.test(stem)) {
+        w = stem;
+        if (rf.test(w)) {
+          w = w + "e";
+        } else if (rg.test(w)) {
+          w = w.replace(re, "");
+        } else if (rh.test(w)) {
+          w = w + "e";
+        }
+      }
+    }
+    // Step 1c
+    if (ri.test(w)) {
+      var fp = ri.exec(w);
+      stem = fp[1];
+      if (s_v.test(stem)) {
+        w = stem + "i";
+      }
+    }
+    // Step 2;
+    if (rj.test(w)) {
+      var fp = rj.exec(w);
+      stem = fp[1];
+      suffix = fp[2];
+      if (mgr0.test(stem)) {
+        w = stem + step2list[suffix];
+      }
+    }
+    // Step 3
+    if (rk.test(w)) {
+      var fp = rk.exec(w);
+      stem = fp[1];
+      suffix = fp[2];
+      if (mgr0.test(stem)) {
+        w = stem + step3list[suffix];
+      }
+    }
+    // Step 4
+    if (rl.test(w)) {
+      var fp = rl.exec(w);
+      stem = fp[1];
+      if (mgr1.test(stem)) {
+        w = stem;
+      }
+    } else if (rm.test(w)) {
+      var fp = rm.exec(w);
+      stem = fp[1] + fp[2];
+      if (mgr1.test(stem)) {
+        w = stem;
+      }
+    }
+    // Step 5
+    if (rn.test(w)) {
+      var fp = rn.exec(w);
+      stem = fp[1];
+      if (mgr1.test(stem) || (meq1.test(stem) && !(rh.test(stem)))) {
+        w = stem;
+      }
+    }
+    if (ro.test(w) && mgr1.test(w)) {
+      w = w.replace(re, "");
+    }
+    // and turn initial Y back to y
+    if (firstch == "y") {
+      w = firstch.toLowerCase() + w.substr(1);
+    }
+    return w;
+  };
+  return langEn;
 })();
--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -4,9 +4,9 @@ The client-side search functionality for this static website
 Using a customized version of Okapi BM25 algorithm:
 
 BM25(D, Q) = Σ i = 1 [
-	((((k1 + 1) * f(qi, D)) * ln((N - n(qi) + 1/2) / (n(qi) + 1/2) + 1) * (k2 + 1)) /
-	(k1 * ((1 - b) + b * (|D| / avgdl)) + f(qi, D) + k2)) *
-	(((k3 + 1) + qf(qi, Q)) / (k3 + qf(qi, Q) + 1))
+  ((((k1 + 1) * f(qi, D)) * ln((N - n(qi) + 1/2) / (n(qi) + 1/2) + 1) * (k2 + 1)) /
+  (k1 * ((1 - b) + b * (|D| / avgdl)) + f(qi, D) + k2)) *
+  (((k3 + 1) + qf(qi, Q)) / (k3 + qf(qi, Q) + 1))
 ]
 
 Free parameters are:
@@ -33,22 +33,22 @@ component saturation for long documents.
 Old implementation I used:
 
 BM25(D, Q) = Σ i = 1 [
-	(((k1 + 1) * f(qi, D)) / (k1 * ((1 - b) + b * (|D| / avgdl)) + f(qi, D))) *
-	ln((N - n(qi) + 1/2) / (n(qi) + 1/2) + 1) *
-	(((k3 + 1) + qf(qi, Q)) / (k3 + qf(qi, Q) + 1))
+  (((k1 + 1) * f(qi, D)) / (k1 * ((1 - b) + b * (|D| / avgdl)) + f(qi, D))) *
+  ln((N - n(qi) + 1/2) / (n(qi) + 1/2) + 1) *
+  (((k3 + 1) + qf(qi, Q)) / (k3 + qf(qi, Q) + 1))
 ]
 */
 
 (function(factory) {
-	"use strict";
-	var loaded = false;
-	function fn() {
-		if (loaded) { return; }
-		loaded = true;
-		factory();
-	}
-	window.onload = fn;
-	document.addEventListener("DOMContentLoaded", fn)
+  "use strict";
+  var loaded = false;
+  function fn() {
+    if (loaded) { return; }
+    loaded = true;
+    factory();
+  }
+  window.onload = fn;
+  document.addEventListener("DOMContentLoaded", fn)
 })(function() {
 "use strict";
 
@@ -65,88 +65,88 @@ var searchInput = document.getElementById("search-bar");
 msg.innerHTML = "Search here!";
 
 function searchDocs(query) {
-	if (!languageData) { return; }
-	// split tokens
-	var tokens = languageData.split(query);
-	// repititions of each stems
-	var queryTermsFreq = {};
-	// query terms set to process
-	var querySets = [];
+  if (!languageData) { return; }
+  // split tokens
+  var tokens = languageData.split(query);
+  // repititions of each stems
+  var queryTermsFreq = {};
+  // query terms set to process
+  var querySets = [];
 
-	// pre-process query tokens first
-	for (var i = 0; i < tokens.length; i++) {
-		var tok = tokens[i];
-		// skip stopwords
-		if (languageData.stop(tok)) { continue; }
-		// stem word
-		var stem = languageData.stem(tok);
-		// increment counters
-		queryTermsFreq[stem] = (queryTermsFreq[stem] || 0) + 1;
-		// add to list of terms to process
-		if (!querySets.includes(stem)) { querySets.push(stem); }
-	}
+  // pre-process query tokens first
+  for (var i = 0; i < tokens.length; i++) {
+    var tok = tokens[i];
+    // skip stopwords
+    if (languageData.stop(tok)) { continue; }
+    // stem word
+    var stem = languageData.stem(tok);
+    // increment counters
+    queryTermsFreq[stem] = (queryTermsFreq[stem] || 0) + 1;
+    // add to list of terms to process
+    if (!querySets.includes(stem)) { querySets.push(stem); }
+  }
 
-	// cached tf normalization factors for eache processed documents
-	var cachedNormFactors = {};
-	// map of relevances
-	var relevances = {};
+  // cached tf normalization factors for eache processed documents
+  var cachedNormFactors = {};
+  // map of relevances
+  var relevances = {};
 
-	// calculate relevance
-	for (var i = 0; i < querySets.length; i++) {
-		var term = querySets[i];
-		var docList = searchIndex.terms[term];
+  // calculate relevance
+  for (var i = 0; i < querySets.length; i++) {
+    var term = querySets[i];
+    var docList = searchIndex.terms[term];
 
-		// no document found indexed containing this term
-		if (!docList) { continue; }
+    // no document found indexed containing this term
+    if (!docList) { continue; }
 
-		var docsContainTerm = docList.length;
+    var docsContainTerm = docList.length;
 
-		// the inverse document frequency of this term over the documents
-		var inverseDocFreq = Math.log(
-				(searchIndex.corpusSize - docsContainTerm + 0.5) /
-				(docsContainTerm + 0.5) + 1
-			);
-		// boost for query
-		var queryBoost = ((k3 + 1) * queryTermsFreq[term]) / (k3 + queryTermsFreq[term]);
+    // the inverse document frequency of this term over the documents
+    var inverseDocFreq = Math.log(
+        (searchIndex.corpusSize - docsContainTerm + 0.5) /
+        (docsContainTerm + 0.5) + 1
+      );
+    // boost for query
+    var queryBoost = ((k3 + 1) * queryTermsFreq[term]) / (k3 + queryTermsFreq[term]);
 
-		for (var j = 0; j < docsContainTerm; j++) {
-			var docTermInfo = docList[j]; // [ doc number, term freq on doc ]
-			var termFreq = docTermInfo[1]; // f(qi, D)
-			var docLength = searchIndex.docs[docTermInfo[0]][1]; // |D|
-			var docId = searchIndex.docs[docTermInfo[0]][0]; // doc hash id on dataIndex
+    for (var j = 0; j < docsContainTerm; j++) {
+      var docTermInfo = docList[j]; // [ doc number, term freq on doc ]
+      var termFreq = docTermInfo[1]; // f(qi, D)
+      var docLength = searchIndex.docs[docTermInfo[0]][1]; // |D|
+      var docId = searchIndex.docs[docTermInfo[0]][0]; // doc hash id on dataIndex
 
-			// get the term frequency normalization factor for this document
-			var normFactor = typeof cachedNormFactors[docTermInfo[0]] == "number"
-				? cachedNormFactors[docTermInfo[0]]
-				: cachedNormFactors[docTermInfo[0]] =
-				(k1 * ((1 - b) + (b * (docLength / searchIndex.avgdl))));
+      // get the term frequency normalization factor for this document
+      var normFactor = typeof cachedNormFactors[docTermInfo[0]] == "number"
+        ? cachedNormFactors[docTermInfo[0]]
+        : cachedNormFactors[docTermInfo[0]] =
+        (k1 * ((1 - b) + (b * (docLength / searchIndex.avgdl))));
 
-			// compute for the saturated tf component, with idf at numerator and applied precision
-			// issue workaround, and multiplied with query frequency for boost
-			relevances[docId] = (relevances[docId] || 0) +
-				(((k1 + 1) * termFreq * inverseDocFreq * (k2 + 1)) /
-				(normFactor + termFreq + k2)) * queryBoost;
-		}
-	}
+      // compute for the saturated tf component, with idf at numerator and applied precision
+      // issue workaround, and multiplied with query frequency for boost
+      relevances[docId] = (relevances[docId] || 0) +
+        (((k1 + 1) * termFreq * inverseDocFreq * (k2 + 1)) /
+        (normFactor + termFreq + k2)) * queryBoost;
+    }
+  }
 
-	var searchResult = [];
+  var searchResult = [];
 
-	// we need an array
-	for (var docId in relevances) {
-		if (Object.hasOwnProperty.call(relevances, docId)) {
-			searchResult.push({
-				identifier: docId,
-				relevance: relevances[docId],
-			});
-		}
-	}
+  // we need an array
+  for (var docId in relevances) {
+    if (Object.hasOwnProperty.call(relevances, docId)) {
+      searchResult.push({
+        identifier: docId,
+        relevance: relevances[docId],
+      });
+    }
+  }
 
-	// sort the result
-	searchResult.sort(function(a, b) {
-		return b.relevance - a.relevance;
-	});
+  // sort the result
+  searchResult.sort(function(a, b) {
+    return b.relevance - a.relevance;
+  });
 
-	return searchResult;
+  return searchResult;
 }
 
 // make this function accessible
@@ -156,63 +156,63 @@ docUtil.search = searchDocs;
 var q = docUtil.query["q"];
 
 if (q && q.length) {
-	// let's show this to user
-	searchInput.value = q;
-	// put it on the title
-	document.title = q + " \u2012 VYT Docs Search";
+  // let's show this to user
+  searchInput.value = q;
+  // put it on the title
+  document.title = q + " \u2012 VYT Docs Search";
 
-	// wait for the search
-	(function waitSearch() {
-		// wait for search index to load
-		msg.innerHTML = "Loading index...";
-		if (!window.searchIndex || !window.dataIndex) { return setTimeout(waitSearch, 1000); }
+  // wait for the search
+  (function waitSearch() {
+    // wait for search index to load
+    msg.innerHTML = "Loading index...";
+    if (!window.searchIndex || !window.dataIndex) { return setTimeout(waitSearch, 1000); }
 
-		// do search
-		msg.innerHTML = "Searching...";
-		// let's time it to show the search speed
-		var start = Date.now();
-		var result = searchDocs(q);
-		var delta = (Date.now() - start) / 1000;
+    // do search
+    msg.innerHTML = "Searching...";
+    // let's time it to show the search speed
+    var start = Date.now();
+    var result = searchDocs(q);
+    var delta = (Date.now() - start) / 1000;
 
-		if (!result.length) {
-			msg.innerHTML = "No result found. Try checking your spelling?";
-			return;
-		}
+    if (!result.length) {
+      msg.innerHTML = "No result found. Try checking your spelling?";
+      return;
+    }
 
-		// display to users
-		var idx = 0;
+    // display to users
+    var idx = 0;
 
-		function displayResults() {
-			if (idx >= result.length) { return; }
-			var item = result[idx++];
-			var info = dataIndex[item.identifier];
+    function displayResults() {
+      if (idx >= result.length) { return; }
+      var item = result[idx++];
+      var info = dataIndex[item.identifier];
 
-			// setup display item
-			var block = document.createElement("div");
-			var title = document.createElement("h3");
-			var anchor = document.createElement("a");
-			var description = document.createElement("p");
-			block.classList.add("search-item");
-			title.classList.add("search-item-title");
-			description.classList.add("search-item-description");
-			title.appendChild(anchor);
-			block.appendChild(title);
-			block.appendChild(description);
-			anchor.appendChild(document.createTextNode(info.title));
-			description.appendChild(document.createTextNode(info.about));
-			anchor.href = info.location + ".html?h=" + encodeURIComponent(docUtil.query["q"]);
+      // setup display item
+      var block = document.createElement("div");
+      var title = document.createElement("h3");
+      var anchor = document.createElement("a");
+      var description = document.createElement("p");
+      block.classList.add("search-item");
+      title.classList.add("search-item-title");
+      description.classList.add("search-item-description");
+      title.appendChild(anchor);
+      block.appendChild(title);
+      block.appendChild(description);
+      anchor.appendChild(document.createTextNode(info.title));
+      description.appendChild(document.createTextNode(info.about));
+      anchor.href = info.location + ".html?h=" + encodeURIComponent(docUtil.query["q"]);
 
-			// add to results
-			resultDisplay.appendChild(block);
+      // add to results
+      resultDisplay.appendChild(block);
 
-			// delay next item to avoid lag in UI
-			setTimeout(displayResults, 15);
-		}
+      // delay next item to avoid lag in UI
+      setTimeout(displayResults, 15);
+    }
 
-		displayResults();
+    displayResults();
 
-		msg.innerHTML = "Found " + result.length + " result(s) in " + delta + " seconds!";
-	})();
+    msg.innerHTML = "Found " + result.length + " result(s) in " + delta + " seconds!";
+  })();
 }
 
 });
