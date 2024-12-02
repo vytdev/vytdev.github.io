67f556d960afaaee1977b6777c0e2dfbc98e7d93
Vincent Yanzee J. Tan <vytdev@gmail.com>
2024-12-02 16:22:41 +0800

    deploy: head

--- assets/linkIndex.js
+++ assets/linkIndex.js
@@ -1,1 +1,1 @@
-window.linkIndex={"2scQAj":"terms.html","27sqpu":"credits.html","35oL02":"cookie.html","3GtdMn":"license.html","3Jv0ml":"disclaimer.html","41Czsx":"about.html","2BYYNc":"contact.html","2Kg9Ho":"privacy.html","4on27w":"index.html"};
+window.linkIndex={"41Czsx":"about.html","2BYYNc":"contact.html","35oL02":"cookie.html","27sqpu":"credits.html","3Jv0ml":"disclaimer.html","4on27w":"index.html","3GtdMn":"license.html","2Kg9Ho":"privacy.html","2scQAj":"terms.html"};
