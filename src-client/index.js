810bfbbef993097f6548b2747f9459f1f72b1114
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-04-11 12:41:42 +0800

    updated search indexing

--- src-client/index.js
+++ src-client/index.js
@@ -1,17 +1,8 @@
 const util = require('./util.js');
 const query = require('./query.js');
 const events = require('./events.js');
-const search = require('./search/index.js');
 
 events.globalEvents.on('load', () => {
   if (query['h'])
     util.highlight(query['h'], document.body);
-  util.changeTheme(util.THEMES.light);
-  search.loadIndex().then(() => {
-    try {
-      alert(JSON.stringify(search.query(prompt('search'))));
-    } catch (e) {
-      alert(e);
-    }
-  });
 });
--- src-client/search/index.js
+++ src-client/search/index.js
@@ -3,21 +3,45 @@ const finder = require('./finder.js');
 const config = require('../config.js');
 const util = require('../util.js');
 
-/**
- * Returns whether the search index is ready.
- * @returns True if the index is ready.
- */
-function isIndexReady() {
-  return typeof searchIndex != 'undefined';
-}
-
 
 /**
  * Load the search index.
  * @returns A Promise which's resolved once the index is loaded.
  */
 function loadIndex() {
-  return util.loadScript(pageInfo.relRoot + config.INDEX_SEARCH);
+  return util.loadScript(pageInfo.relRoot + config.INDEX_SEARCH)
+    .then(() => {
+      searchIndex.ref2term = Object.create(null);
+      for (const [ term, ref ] in Object.entries(searchIndex.term2ref))
+        searchIndex.ref2term[ref] = term;
+    })
+    .then(() => {
+      exports.searchIndex = searchIndex;
+    });
+}
+
+
+/**
+ * Loads the page data index.
+ * @returns A Promise.
+ */
+function loadPageDataIndex() {
+  return util.loadScript(config.INDEX_PAGEDATA)
+    .then(() => {
+      exports.pageDataIndex = pageDataIndex;
+    });
+}
+
+
+/**
+ * Loads the link index.
+ * @returns A Promise.
+ */
+function loadLinkIndex() {
+  return util.loadScript(config.INDEX_LINKS)
+    .then(() => {
+      exports.linkIndex = linkIndex;
+    })
 }
 
 
@@ -26,7 +50,7 @@ function loadIndex() {
  * @returns The index or null.
  */
 function getSearchIndex() {
-  if (isIndexReady())
+  if (exports.searchIndex)
     return searchIndex;
   return null;
 }
@@ -38,19 +62,50 @@ function getSearchIndex() {
  * @returns An array of documents, or null.
  */
 function query(text) {
-  if (!isIndexReady())
+  if (!exports.searchIndex || !exports.pageDataIndex)
     return null;
   return Object.entries(finder.performSearch(text))
     .map(([docRef, score]) => ({
       docUid: docRef,
       relevance: score,
+      pageInfo: pageDataIndex[docRef],
     }));
 }
 
 
+/**
+ * Get authors list.
+ * @returns An array of author username strings
+ */
+function getAuthorsList() {
+  if (!exports.searchIndex)
+    return null;
+  return Object.keys(searchIndex.authors)
+    .map(v => searchIndex.ref2term[v]);
+}
+
+
+/**
+ * Get tags list.
+ * @returns An array of tags.
+ */
+function getTagsList() {
+  if (!exports.searchIndex)
+    return null;
+  return Object.entries(searchIndex.tags)
+    .map(v => searchIndex.ref2term[v]);
+}
+
+
 module.exports = {
-  isIndexReady,
+  searchIndex:    null,
+  pageDataIndex:  null,
+  linkIndex:      null,
   loadIndex,
+  loadPageDataIndex,
+  loadLinkIndex,
   getSearchIndex,
   query,
+  getAuthorsList,
+  getTagsList,
 };
